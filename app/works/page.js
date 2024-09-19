727685ae8d0baa0a51681343ee8c3eb44be75f97
isiagi <isiagigeofrey0@gmail.com>
2024-09-19 19:52:33 +0300

    chore: Add portfolio

--- app/works/page.js
+++ app/works/page.js
@@ -11,9 +11,16 @@ import "lightgallery/css/lg-thumbnail.css";
 // import plugins if you need
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
+import lgfull from "lightgallery/plugins/fullscreen";
 import Image from "next/image";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function page() {
+  const onInit = () => {
+    console.log("lightGallery has been initialized");
+  };
+
   return (
     <div>
       <div
@@ -38,25 +45,133 @@ function page() {
       </div>
 
       <div>
-        <LightGallery
-          // onInit={onInit}
-          speed={500}
-          plugins={[lgThumbnail, lgZoom]}
-          //   elementClassNames={styles.gallery__flex}
-        >
-          {
-            <Image
-              alt="img1"
-              src={
-                "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
-              }
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: "100%", height: "100%", objectFit: "contain" }}
-            />
-          }
-        </LightGallery>
+        <Tabs defaultValue="account" className="w-full grid">
+          <TabsList className="justify-self-center my-5">
+            <TabsTrigger value="account">Photos</TabsTrigger>
+            <TabsTrigger value="password">Graphics</TabsTrigger>
+            <TabsTrigger value="password">Adverts</TabsTrigger>
+            <TabsTrigger value="password">Branding</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8">
+              <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom, lgfull]}
+                thumbnail={true}
+                rotate={true}
+                hideControlOnEnd={true}
+                elementClassNames="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8"
+                mobileSettings={{
+                  controls: false,
+                  showCloseIcon: false,
+                  download: false,
+                  rotate: false,
+                }}
+                animateThumb={false}
+                zoomFromOrigin={false}
+                allowMediaOverlap={true}
+                toggleThumb={true}
+              >
+                {
+                  <>
+                    <a
+                      href={
+                        "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                      }
+                      data-thumb="https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=100&h=100&auto=format&fit=crop&q=60"
+                      key={1}
+                    >
+                      <Image
+                        alt="img1"
+                        src={
+                          "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                        }
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </a>
+
+                    <a
+                      href={
+                        "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                      }
+                      data-thumb="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                      key={2}
+                    >
+                      <Image
+                        alt="img1"
+                        src={
+                          "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                        }
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </a>
+
+                    {/* <Image
+                      alt="img1"
+                      src={
+                        "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                      }
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "contain",
+                      }}
+                    />
+
+                    <Image
+                      alt="img1"
+                      src={
+                        "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                      }
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "contain",
+                      }}
+                    />
+
+                    <Image
+                      alt="img1"
+                      src={
+                        "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydGZvbGlvJTIwd29ya3N8ZW58MHx8MHx8fDA%3D"
+                      }
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "contain",
+                      }}
+                    /> */}
+                  </>
+                }
+              </LightGallery>
+            </div>
+          </TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
       </div>
     </div>
   );
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -12,7 +12,7 @@ export function NavbarComponent() {
   };
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md fixed w-full z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
