02853055e9f6d7bd48f9a022295fc789af814b15
isiagi <isiagigeofrey0@gmail.com>
2024-09-19 20:51:34 +0300

    chore: Adds about us page.

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import { AboutUsPageComponent } from "@/components/about-us-page";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <AboutUsPageComponent />
+    </div>
+  );
+}
+
+export default page;
